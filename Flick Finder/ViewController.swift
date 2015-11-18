94d26fba0df6ec99a0a92fb68c11f31d6260cb1b
Jeffersonbe <jefferson.bonnaire@gmail.com>
2015-11-18 16:13:39 +0000

    Add search by Longitude and Latitude

--- Flick Finder/ViewController.swift
+++ Flick Finder/ViewController.swift
@@ -63,15 +63,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func searchByPhraseButton(sender: AnyObject) {
-        if let usertext = phraseTextField.text {
-            TEXT = usertext
-        }
-
+        
         /* 2 - API method arguments */
         methodArguments = [
             "method": METHOD_NAME,
             "api_key": API_KEY!,
-            "text": TEXT!,
+            "text": phraseTextField.text!,
             "extras": EXTRAS,
             "format": DATA_FORMAT,
             "nojsoncallback": NO_JSON_CALLBACK
@@ -127,41 +124,46 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
             
             /* GUARD: Are the "photos" and "photo" keys in our result? */
-            guard let photosDictionary = parsedResult["photos"] as? NSDictionary,
-                photoArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
-                    print("Cannot find keys 'photos' and 'photo' in \(parsedResult)")
-                    return
+            guard let   photosDictionary = parsedResult["photos"] as? NSDictionary,
+                        photoArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
+                print("Cannot find keys 'photos' and 'photo' in \(parsedResult)")
+                return
             }
             
-            /* GUARD: How many photos does the API return */
-            guard let total = photosDictionary["pages"]as? Int where total != 0 else {
+            /* GUARD: Is the "total" key in photosDictionary? */
+            guard let totalPhotos = (photosDictionary["total"] as? NSString)?.integerValue else {
+                print("Cannot find key 'total' in \(photosDictionary)")
+                return
+            }
+            
+            if totalPhotos > 0 {
+                /* 7 - Generate a random number, then select a random photo */
+                let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                let photoDictionary = photoArray[randomPhotoIndex] as [String: AnyObject]
+                let photoTitle = photoDictionary["title"] as? String /* non-fatal */
+            
+                /* GUARD: Does our photo have a key for 'url_m'? */
+                guard let imageUrlString = photoDictionary["url_m"] as? String else {
+                    print("Cannot find key 'url_m' in \(photoDictionary)")
+                    return
+                }
+                
+                /* 8 - If an image exists at the url, set the image and title */
+                let imageURL = NSURL(string: imageUrlString)
+                if let imageData = NSData(contentsOfURL: imageURL!) {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.defaultTextLabel.text = ""
+                        self.imageView.image = UIImage(data: imageData)
+                        self.imageTitleLabel.text = photoTitle ?? "(Untitled)"
+                    })
+                } else {
+                    print("Image does not exist at \(imageURL)")
+                }
+            } else {
                 dispatch_async(dispatch_get_main_queue(), {
                     self.imageView.image = nil
                     self.defaultTextLabel.text = "Cannot find photos with \(self.TEXT!)"
                 })
-                return
-            }
-            
-            /* 7 - Generate a random number, then select a random photo */
-            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
-            let photoDictionary = photoArray[randomPhotoIndex] as [String: AnyObject]
-            let photoTitle = photoDictionary["title"] as? String /* non-fatal */
-            
-            /* GUARD: Does our photo have a key for 'url_m'? */
-            guard let imageUrlString = photoDictionary["url_m"] as? String else {
-                print("Cannot find key 'url_m' in \(photoDictionary)")
-                return
-            }
-            
-            /* 8 - If an image exists at the url, set the image and title */
-            let imageURL = NSURL(string: imageUrlString)
-            if let imageData = NSData(contentsOfURL: imageURL!) {
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.imageView.image = UIImage(data: imageData)
-                    self.imageTitleLabel.text = photoTitle ?? "(Untitled)"
-                })
-            } else {
-                print("Image does not exist at \(imageURL)")
             }
         }
         
@@ -170,6 +172,118 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func searchByLocationButton(sender: AnyObject) {
+        let minlongitudeTextField = Int(longitudeTextField.text!)! * -1
+        let minlatitudeTextField =  Int(latitudeTextField.text!)! * -1
+        let maxlongitudeTextField = longitudeTextField.text!
+        let maxlatitudeTextField = latitudeTextField.text!
+        
+        /* 2 - API method arguments */
+        methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY!,
+            "bbox": "\(minlongitudeTextField),\(minlatitudeTextField),\(maxlongitudeTextField),\(maxlatitudeTextField)",
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        
+        print(methodArguments)
+        
+        /* 3 - Initialize session and url */
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(methodArguments!)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        /* 4 - Initialize task for getting data */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* 5 - Check for a successful response */
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* 6 - Parse the data (i.e. convert the data to JSON and look for values!) */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Are the "photos" and "photo" keys in our result? */
+            guard let   photosDictionary = parsedResult["photos"] as? NSDictionary,
+                photoArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
+                    print("Cannot find keys 'photos' and 'photo' in \(parsedResult)")
+                    return
+            }
+            
+            /* GUARD: Is the "total" key in photosDictionary? */
+            guard let totalPhotos = (photosDictionary["total"] as? NSString)?.integerValue else {
+                print("Cannot find key 'total' in \(photosDictionary)")
+                return
+            }
+            
+            if totalPhotos > 0 {
+                /* 7 - Generate a random number, then select a random photo */
+                let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                let photoDictionary = photoArray[randomPhotoIndex] as [String: AnyObject]
+                let photoTitle = photoDictionary["title"] as? String /* non-fatal */
+                
+                /* GUARD: Does our photo have a key for 'url_m'? */
+                guard let imageUrlString = photoDictionary["url_m"] as? String else {
+                    print("Cannot find key 'url_m' in \(photoDictionary)")
+                    return
+                }
+                
+                /* 8 - If an image exists at the url, set the image and title */
+                let imageURL = NSURL(string: imageUrlString)
+                if let imageData = NSData(contentsOfURL: imageURL!) {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.defaultTextLabel.text = ""
+                        self.imageView.image = UIImage(data: imageData)
+                        self.imageTitleLabel.text = photoTitle ?? "(Untitled)"
+                    })
+                } else {
+                    print("Image does not exist at \(imageURL)")
+                }
+            } else {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.imageView.image = nil
+                    self.defaultTextLabel.text = "Cannot find photos with \(self.TEXT!)"
+                })
+            }
+        }
+        
+        /* 9 - Resume (execute) the task */
+        task.resume()
     }
     
     /* Helper function: Given a dictionary of parameters, convert to a string for a url */
@@ -199,16 +313,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        self.view.endEditing(true)
+        view.endEditing(true)
     }
     
-    //MARK: KeyboardManagement
     func keyboardWillShow(notification: NSNotification) {
-        view.frame.origin.y -= getKeyboardHeight(notification)
+        if imageView.image != nil {
+            defaultTextLabel.alpha = 0.0
+        }
+        if view.frame.origin.y == 0.0 {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
     }
     
-    func keyboardWillHide(notification: NSNotification){
-        view.frame.origin.y += getKeyboardHeight(notification)
+    func keyboardWillHide(notification: NSNotification) {
+        if imageView.image == nil {
+            defaultTextLabel.alpha = 1.0
+        }
+        if view.frame.origin.y != 0.0 {
+            view.frame.origin.y += getKeyboardHeight(notification)
+        }
     }
     
     func getKeyboardHeight(notification: NSNotification) -> CGFloat {
@@ -236,5 +359,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
         NSNotificationCenter.defaultCenter().removeObserver(self,
             name:UIKeyboardWillHideNotification,
             object: nil)
+    }
+}
+
+// MARK: - ViewController (Keyboard Fix)
+
+/* This extension was added as a fix based on student comments */
+extension ViewController {
+    func dismissAnyVisibleKeyboards() {
+        if phraseTextField.isFirstResponder() || latitudeTextField.isFirstResponder() || longitudeTextField.isFirstResponder() {
+            self.view.endEditing(true)
+        }
     }
 }
